b857c4dff975e5644656d1fe1b3361f459f4c280
Biola <gbozee@gmail.com>
2016-02-10 09:52:21 +0100

    new geodjango dockerfile

--- geodjango/build_env.sh
+++
@@ -1,8 +0,0 @@
-export APP_NAME=$(basename $(dirname $(find src -name settings.py -type f | head -1)))
-
-export PYTHONPATH=${BASE_DIR}:${BASE_DIR}/src
-# Add site-packages to PYTHONPATH ??
-for l in $(rpm -ql python-libs | grep site-packages |grep -vi readme)
-do
-    export PYTHONPATH=${PYTHONPATH}:${l}
-done
--- geodjango/django_settings.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source ${BASE_DIR}/build_env.sh
-echo "from ${APP_NAME}.settings import *" > site/production.py
-echo "from .base import *"               >> site/production.py
-echo "SECRET_KEY='"$(/usr/bin/pwgen -s -c -n 64 1)"'" >> site/production.py
